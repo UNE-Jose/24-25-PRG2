c8b67dece65eb31804d9ed6881a9f2ae5dab3ca0
User <User>
2025-03-18 14:01:23 +0100

    Cambios

---
+++ JuegoTresEnRaya/src/Coordenada.java
@@ -0,0 +1,15 @@
+package JuegoTresEnRaya.src;
+
+public class Coordenada {
+
+    public void pedir() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pedir'");
+    }
+
+    public boolean esValida() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'esValida'");
+    }
+
+}
